53a5839d4120cfe4fb2c7d8927feb986a4f05837
EiichiMiyagawa <m.monobyte@gmail.com>
2023-12-14 01:12:21 +0900

    不要な処理を削除

--- update.js
+++ update.js
@@ -4,7 +4,6 @@ import { Day, TimeZone } from "https://js.sabae.cc/DateTime.js";
 const domain = Deno.args[0];
 const getLatestHotelApi = domain + Deno.args[1];
 const getLatestRsvSumApi = domain + Deno.args[2];
-const getLatestRsvPrefectureSumApi = domain + Deno.args[3];
 
 const hotelData = await (await fetch(getLatestHotelApi)).json();
 await Deno.writeTextFile("latest_hotel.csv", CSV.stringify(hotelData));
@@ -16,6 +15,3 @@ const backupLatestRsvSumData = latestRsvSumData.filter(d => {
     return d.date_visit >= new Day(TimeZone.JST).toString()
 });
 await Deno.writeTextFile("data/" + new Day(TimeZone.JST).toString() + ".csv", CSV.stringify(backupLatestRsvSumData));
-
-// const latestRsvPrefectureSumData = await (await fetch(getLatestRsvPrefectureSumApi)).json();
-// await Deno.writeTextFile("latest_rsv_prefecture_sum.csv", CSV.stringify(latestRsvPrefectureSumData));
